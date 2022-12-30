3425ee219ba171e905f69b09067925256e1d1b72
Bneeruga <89408991+Bneeruga@users.noreply.github.com>
2022-12-30 16:34:24 -0500

    Add files via upload

---
+++ cloud-gateway/src/test/java/com/firstapi/cloud/gateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstapi.cloud.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
